f97b7e5180258d2e0580431e1307aa550eee0be3
manick <manick.s@chargebee.com>
2021-04-29 13:00:26 +0530

    Rest connector initial commit

---
+++ cb-restconnector/src/test/java/org/cb/restconnector/AppTest.java
@@ -0,0 +1,14 @@
+package java.org.cb.restconnector;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+}
