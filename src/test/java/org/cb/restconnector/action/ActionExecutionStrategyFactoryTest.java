68f6fe1b555fb71f7cb17a794d50f961eaf8f559
manick <manick.s@chargebee.com>
2021-05-04 11:30:24 +0530

    Renamed method name

--- src/test/java/org/cb/restconnector/action/ActionExecutionStrategyFactoryTest.java
+++ src/test/java/org/cb/restconnector/action/ActionExecutionStrategyFactoryTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class ActionExecutionStrategyFactoryTest {
   @Test
-  public void TestActionExecturionStrategyFactory() {
+  public void TestActionExecutionStrategyFactory() {
      Assert.assertTrue(ActionExecutionStrategyFactory.getRestExecutionStrategy(Action.GET) instanceof GetActionRestExecutionStrategy);
      Assert.assertTrue(ActionExecutionStrategyFactory.getRestExecutionStrategy(Action.POST) instanceof PostActionExecutionStrategy);
      Assert.assertTrue(ActionExecutionStrategyFactory.getRestExecutionStrategy(Action.PUT) instanceof PutOrPatchActionExecutionStrategy); ;
