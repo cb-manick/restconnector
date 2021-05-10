e424f6e7f860217b2ae6fbced613aaaca9e8c06e
cb-manick <80755357+cb-manick@users.noreply.github.com>
2021-05-10 18:01:06 +0530

    Open api parser

--- src/test/java/org/cb/restconnector/openapi/OpenApiParserTest.java
+++ src/test/java/org/cb/restconnector/openapi/OpenApiParserTest.java
@@ -1,36 +1,62 @@
 package org.cb.restconnector.openapi;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
-import org.junit.Assert;
 import org.openapi4j.core.exception.EncodeException;
 import org.openapi4j.core.exception.ResolutionException;
 import org.openapi4j.core.validation.ValidationException;
 import org.openapi4j.parser.OpenApi3Parser;
-import org.openapi4j.parser.model.v3.Components;
-import org.openapi4j.parser.model.v3.OpenApi3;
-import org.openapi4j.parser.model.v3.Path;
+import org.openapi4j.parser.model.v3.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 import java.util.Map;
 
-
 public class OpenApiParserTest {
-    @Test
-    public void testOpenApiParser() throws MalformedURLException, ResolutionException, ValidationException, EncodeException {
+  @Test
+  public void testOpenApiParser()
+      throws MalformedURLException, ResolutionException, ValidationException, EncodeException {
 
-        OpenApi3 api = new OpenApi3Parser().parse(new URL("https://petstore3.swagger.io/api/v3/openapi.json"), false);
-        //"https://petstore3.swagger.io/api/v3/openapi.json", null, null);
+    OpenApi3 api =
+        new OpenApi3Parser()
+            .parse(new URL("https://petstore3.swagger.io/api/v3/openapi.json"), false);
+    // "https://petstore3.swagger.io/api/v3/openapi.json", null, null);
 
-        Components components = api.getComponents();//components
-        Map<String, Path> paths = api.getPaths();
+    Components components = api.getComponents(); // components
 
-        for ( String path : paths.keySet()) {
-            Path pathObj = paths.get(path);
-            JsonNode jsonNode = pathObj.toNode();
-            System.out.println(jsonNode.toPrettyString());
+    Map<String, Path> paths = api.getPaths();
 
+    for (String path : paths.keySet()) {
+
+      Path pathObj = paths.get(path);
+      Map<String, Operation> operations = pathObj.getOperations();
+      for (String operation : operations.keySet()) {
+        Operation operation1 = operations.get(operation);
+        System.out.println(operation1.getOperationId());
+        List<Parameter> parameters = operation1.getParameters();
+        if (parameters != null) {
+          System.out.println(parameters.toString());
         }
+        RequestBody requestBody = operation1.getRequestBody();
+        if (requestBody != null) {
+          MediaType contentMediaType = requestBody.getContentMediaType("application/json");
+          if (contentMediaType != null) {
+            String ref = contentMediaType.getSchema().getRef();
+            if (ref != null) {
+              String[] split = ref.split("/");
+              Schema schema = components.getSchema(split[split.length - 1]);
+              if (schema != null) {
+                System.out.println(schema.toNode().toPrettyString());
+              }
+            }
+          }
+        }
+        //  requestBody.toNode();
+      }
+      System.out.println("---");
+      //      JsonNode jsonNode = pathObj.toNode();
+      //      System.out.println(jsonNode.toPrettyString());
+      //      System.out.println("---");
     }
+  }
 }
